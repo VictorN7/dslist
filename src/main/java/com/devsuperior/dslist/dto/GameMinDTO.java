e3dc747147251584db206aad78b41033581252b7
Victor Nogueira <victorhnogueira@hotmail.com>
2025-08-21 01:08:26 -0300

    GameMinDTO, GameRepository, GameService, GameController

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id; 
+	private String title; 
+	private Integer year ; 
+	private String imgurl;
+	private Double score;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+		
+	}
+
+	public GameMinDTO(Game identity) {
+		super();
+		id = identity.getId();
+		title = identity.getTitle();
+		year = identity.getYear();
+		imgurl = identity.getImgUrl();
+		score = identity.getScore();
+		shortDescription = identity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgurl() {
+		return imgurl;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+}
